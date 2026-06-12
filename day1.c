f601070805a615cb32a475b292b0b0a642e5c8ff
Khushi Thakur <ajeetthakur22580@gmail.com>
2026-06-12 20:33:15 +0530

    message

---
+++ day1.c
@@ -0,0 +1,13 @@
+// sum of first n number
+#include <stdio.h>
+
+int main()
+{
+    int n, sum = 0;
+    printf("enter number:");
+    scanf("%d", &n);
+    for (int i = 1; i <= n; i++)
+        sum = sum + i;
+    printf("sum=%d", sum);
+    return 0;
+}
