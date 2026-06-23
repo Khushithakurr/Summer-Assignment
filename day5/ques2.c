2f8788fbb45dbbfb33fee5f737f5c5d74ba168ce
Khushithakurr <ajeetthakur22580@gmail.com>
2026-06-23 20:09:13 +0530

    Add files via upload

---
+++ day5/ques2.c
@@ -0,0 +1,31 @@
+// to check strong number
+#include <stdio.h>
+
+int main()
+{
+    int n, temp,i, digit, sum = 0;
+    int fact;
+    printf("enter the number");
+    scanf("%d", &n);
+    temp = n;
+    while (temp > 0)
+    {
+        digit = temp % 10;
+        fact = 1;
+        for ( i = 1; i <= digit; i++)
+        {
+            fact = fact * i;
+        }
+        sum = sum + fact;
+        temp = temp / 10;
+    }
+    if (sum == n)
+    {
+        printf("%d is a strong number", n);
+    }
+    else
+    {
+        printf("%d is not a strong number", n);
+    }
+    return 0;
+}
