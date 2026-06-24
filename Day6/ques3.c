49a0dbe66e413cedd710b904fcd3a224b9d7a380
Khushithakurr <ajeetthakur22580@gmail.com>
2026-06-24 21:18:19 +0530

    Add files via upload

---
+++ Day6/ques3.c
@@ -0,0 +1,16 @@
+ // to count set bits in number
+#include <stdio.h>
+
+    int main()
+{
+    int n, count = 0;
+    printf("enter the number");
+    scanf("%d", &n);
+    while (n > 0)
+    {
+        count = count +( n & 1);
+        n = n >> 1;
+    }
+    printf("number of set bits=%d", count);
+    return 0;
+}
