0c4ca983cfcd42134ec28ab0f9c8f0bc8d9ac8e0
Khushithakurr <ajeetthakur22580@gmail.com>
2026-06-25 21:44:48 +0530

    Add files via upload

---
+++ Day7/ques3.c
@@ -0,0 +1,16 @@
+// recursive sum of digits
+#include <stdio.h>
+int sumdigits(int n)
+{
+    if (n == 0)
+        return 0;
+    return (n % 10) + sumdigits(n / 10);
+}
+int main()
+{
+    int n;
+    printf("enter the number");
+    scanf("%d", &n);
+    printf("sum of digits=%d", sumdigits(n));
+    return 0;
+}
