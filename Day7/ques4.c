2753b09f0d1f7b87881b4873afa05c01510078f3
Khushithakurr <ajeetthakur22580@gmail.com>
2026-06-25 21:51:22 +0530

    Add files via upload

---
+++ Day7/ques4.c
@@ -0,0 +1,16 @@
+// recursive reverse number
+#include <stdio.h>
+int reverse(int n, int rev)
+{
+    if (n == 0)
+        return rev;
+    return reverse(n / 10, rev * 10 + n % 10);
+}
+int main()
+{
+    int n;
+    printf("enter the number");
+    scanf("%d", &n);
+    printf("reverse of %d=%d", n, reverse(n, 0));
+    return 0;
+}
