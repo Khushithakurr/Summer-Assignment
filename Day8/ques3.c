a6e9768ad8d4ffe258298f69a1688d901ec487f5
Khushithakurr <ajeetthakur22580@gmail.com>
2026-06-26 12:04:09 +0530

    Add files via upload

---
+++ Day8/ques3.c
@@ -0,0 +1,18 @@
+// character triangle
+#include <stdio.h>
+
+int main()
+{
+    int n, i, j;
+    printf("enter the no of rows");
+    scanf("%d", &n);
+    for (i = 1; i <= n; i++)
+    {
+        for (j = 0; j < i; j++)
+        {
+            printf("%c",'A'+ j);
+        }
+        printf("\n");
+    }
+    return 0;
+}
