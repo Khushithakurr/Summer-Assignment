192c9da6546a9456670296f4ee1ffaa4419ecfdd
Khushithakurr <ajeetthakur22580@gmail.com>
2026-06-29 09:33:18 +0530

    Add files via upload

---
+++ Day11/ques42.c
@@ -0,0 +1,17 @@
+// functiom to find the max of two numbers
+#include <stdio.h>
+int max(int a, int b)
+{
+    if (a > b)
+        return a;
+    else
+        return b;
+}
+int main()
+{
+    int a, b;
+    printf("enter two numbers");
+    scanf("%d %d", &a, &b);
+    printf("max no=%d", max(a, b));
+    return 0;
+}
